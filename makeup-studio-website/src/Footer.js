415fc087676418479a6d651dc4cce49950e77aba
Reinhardt Erasmus <reinhardt@talentdigital.art>
2024-06-25 19:47:04 +0200

    chore: Add Footer component with social media links

---
+++ makeup-studio-website/src/Footer.js
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+      <footer>
+        <p>© 2024 Permanent Make-Up Studio</p>
+        <div>
+          <a href="https://facebook.com">Facebook</a>
+          <a href="https://instagram.com">Instagram</a>
+          <a href="https://twitter.com">Twitter</a>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
