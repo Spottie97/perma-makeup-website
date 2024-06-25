9eb60e4afc256b655e16f43a54fe90755df6376b
Reinhardt Erasmus <reinhardt@talentdigital.art>
2024-06-25 19:47:31 +0200

    feat: Add Home component with welcome message

---
+++ makeup-studio-website/src/Home.js
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+      <div>
+        <h1>Welcome to Permanent Make-Up Studio</h1>
+        <p>Your beauty, our passion.</p>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
