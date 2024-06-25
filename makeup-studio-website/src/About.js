adce5b0b319f79acde16ca29bff81d91388a29e5
Reinhardt Erasmus <reinhardt@talentdigital.art>
2024-06-25 19:50:11 +0200

    chore: Add About component with information about the studio and the artist

---
+++ makeup-studio-website/src/About.js
@@ -0,0 +1,11 @@
+function About() {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>Information about the studio and the artist.</p>
+      </div>
+    );
+  }
+  
+  export default About;
+  
